2a5cbd8aa0c8ab22e57b929fb28faba5ce615a3b
sudaisansari <ansarisudais333@gmail.com>
2024-03-28 18:37:22 +0500

    Changes again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Services from "@/components/widgets/Services";
-import Hero from "../components/widgets/Hero";
+import Hero from "@/components/widgets/Hero";
+import { InfiniteMovingCardsDemo } from "@/components/widgets/ServicesMoving";
 
 export default function Home() {
   return (
    <main>
     <Hero />
     <Services />
+    <InfiniteMovingCardsDemo />
    </main>
   )
 }
---
+++ src/components/widgets/ServicesMoving.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+
+export function InfiniteMovingCardsDemo() {
+  return (
+    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <InfiniteMovingCards
+        direction="right"
+        speed="slow"
+      />
+    </div>
+  );
+}
